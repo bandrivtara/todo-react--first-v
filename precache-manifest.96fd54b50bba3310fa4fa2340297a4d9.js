d8341c75bc97c471e7af453a02559d09a3948ee5
Taras Bandrivskyy <bandrivtara@gmail.com>
2020-03-10 17:18:30 +0100

    push on git

---
+++ precache-manifest.96fd54b50bba3310fa4fa2340297a4d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79bf8ad45de96c67fc1eb86db55ae872",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb920592fac363e8aa7",
+    "url": "/static/css/main.49b9ca60.chunk.css"
+  },
+  {
+    "revision": "87c0972a2fc104500b49",
+    "url": "/static/js/2.543234b8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.543234b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb920592fac363e8aa7",
+    "url": "/static/js/main.b2a6ab1b.chunk.js"
+  },
+  {
+    "revision": "8a65f322e8102ff31103",
+    "url": "/static/js/runtime-main.3542f72c.js"
+  },
+  {
+    "revision": "16a95679130d212b156729b8c25c1e84",
+    "url": "/static/media/drawing.16a95679.svg"
+  },
+  {
+    "revision": "c9bc67026dd14522e5d459f43f0852c1",
+    "url": "/static/media/note.c9bc6702.png"
+  },
+  {
+    "revision": "54a50b679b1edf82e991535b13df3418",
+    "url": "/static/media/note_usun.54a50b67.svg"
+  }
+]);
